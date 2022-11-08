bf4615784ce9b1262440f721dca7c9641dc02c2d
Lukáš Daněk <dr.danek@gmail.com>
2022-11-08 19:44:29 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import AES from 'crypto-js/aes'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TPeers = {
+  name: string
+  index: string
 }
 
-export default App;
+function App() {
+  const socketRef = useRef<WebSocket>()
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const [name, setName] = useState('')
+
+  const idRef = useRef('')
+
+  const [peers, setPeers] = useState<TPeers[]>([])
+
+  // @ts-ignore
+  const setMessageEvent = useCallback(() => {
+    if (socketRef.current?.onopen === null) {
+      socketRef.current?.addEventListener('open', event => {
+        start()
+      })
+    }
+    if (socketRef.current?.onclose === null) {
+      socketRef.current?.addEventListener('close', event => {
+        initSocket()
+      })
+    }
+    if (socketRef.current?.onerror === null) {
+      socketRef.current?.addEventListener('error', event => {
+        initSocket()
+      })
+    }
+    if (socketRef.current?.onmessage === null) {
+      socketRef.current?.addEventListener('message', event => {
+        const passed = JSON.parse(event.data)
+        const value: Record<string, string> = passed
+        const keys = Object.keys(value)
+        if (!keys.includes('do')) {
+          return
+        }
+        switch (value.do) {
+          case 'peers':
+            const found = (passed.peers as TPeers[]).find(one => one.index === idRef.current)
+            const sentName = found && found.name ? found.name : ''
+            if (name !== sentName) setName(sentName)
+            setPeers(passed.peers)
+            break
+          case 'pong':
+            break
+          case 'reset':
+            newId('')
+            socketSend(
+              JSON.stringify({
+                do: 'reset',
+                index: idRef.current,
+                room: passed.room,
+              })
+            )
+            break
+        }
+      })
+    }
+  })
+
+  // @ts-ignore
+  const initSocket = useCallback(() => {
+    if (!socketRef.current || socketRef.current?.readyState > 1) {
+      try {
+        socketRef.current = new WebSocket('wss://localhost:8100/websockets')
+        if (socketRef.current) setMessageEvent()
+      } catch (Error) {
+        console.error('WebSocket not available')
+      }
+      return true
+    }
+    return false
+    // @ts-ignore
+  }, [setMessageEvent])
+
+  const newId = async (name: string) => {
+    const useName = name !== '' ? name : undefined
+    const id = AES.encrypt(
+      useName || Math.floor(Math.random() * 1000).toString(16),
+      Math.ceil(Date.now() / Math.random()).toString()
+    ).toString()
+    idRef.current = id
+  }
+
+  const socketSend = useCallback(
+    (message: string) => {
+      if (!socketRef.current || socketRef.current?.readyState > 1) {
+        initSocket()
+      }
+
+      if (socketRef.current?.readyState === 1) {
+        socketRef.current?.send(message)
+      }
+    },
+    [initSocket]
+  )
+
+  const start = useCallback(async () => {
+    if (idRef.current === '') await newId(name)
+    socketSend(JSON.stringify({ do: 'ping', index: idRef.current }))
+  }, [socketSend])
+
+  useEffect(() => {
+    initSocket()
+  }, [])
+
+  return (
+    <div className='App'>
+      {name === '' && (
+        <form
+          onSubmit={event => {
+            event.preventDefault()
+            setName(inputRef.current?.value || '')
+            socketSend(
+              JSON.stringify({
+                do: 'nick',
+                index: idRef.current,
+                name: inputRef.current?.value || '',
+              })
+            )
+          }}
+        >
+          <label htmlFor='nick'>Jméno: </label>
+          <input name='nick' type='text' ref={inputRef} />
+          <button type='submit'>použít</button>
+        </form>
+      )}
+      {peers.map((one, idx) => (
+        <div key={idx}>{one.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App
